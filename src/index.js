65e6fed6dc4e07f7e85e6707d07cf586a703e4e0
Avadh Thummar <91778246+avadhthummar@users.noreply.github.com>
2023-10-25 23:15:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 //Write your code here
-class API{
-  constructor(url, method = 'GET'){
-      this.url = url;  
-      this.method = method;
-      this.#secure = url.startswith("https");
-  }
-  isSecure(){
-    return this.#secure
-  }
-  updateURl(url){
+class API {
+  constructor(url, method = 'GET') {
     this.url = url;
-    this.#secure = url.startswith("https");
+    this.method = method;
+    this.#secure = url.startsWith('https');
+  }
+
+  isSecure() {
+    return this.#secure;
+  }
+
+  updateUrl(newUrl) {
+    this.url = newUrl;
+    this.#secure = newUrl.startsWith('https');
   }
 }
 
-
-
-
-
-//Do not remove this code
-module.exports = { API }
+// Export the API class
+module.exports = {API};
