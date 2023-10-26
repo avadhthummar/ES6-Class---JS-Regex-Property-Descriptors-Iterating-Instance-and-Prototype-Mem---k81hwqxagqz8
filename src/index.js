7a46a9cb3cefe12212caeee6ef5d38ebb0f0ef6c
Avadh Thummar <91778246+avadhthummar@users.noreply.github.com>
2023-10-26 21:08:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 //Write your code here
 class API {
+  #secure
   constructor(url, method = 'GET') {
     this.url = url;
     this.method = method;
-    this.#secure = url.startsWith('https');
+    this.#secure = url.startsWith("https");
   }
 
   isSecure() {
     return this.#secure;
   }
 
-  updateUrl(newUrl) {
-    this.url = newUrl;
-    this.#secure = newUrl.startsWith('https');
+  updateUrl(url) {
+    this.url = Url;
+    this.#secure = Url.startsWith("https");
   }
 }
 
